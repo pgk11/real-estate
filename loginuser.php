4c250c8ad7b47c9cab7807b7c4683f1fa85b6b27
pgk11 <43056043+pgk11@users.noreply.github.com>
2024-12-08 13:07:57 +0000

    Update loginuser.php
    Added recaptcha implementation, password and account lockout policy, under process

--- loginuser.php
+++ loginuser.php
@@ -8,6 +8,7 @@
         return $data;
       }
     $username='';$password='';$b=true;
+	$attemptcount = 0;
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
         if(isset($_POST['username']))
@@ -16,6 +17,15 @@
         if(isset($_POST['password']))
 				$password=test_input($_POST['password']);
 				else $b=false;
+		//secretkey of google recaptcha
+		$secretkey = "6Le8G5QqAAAAAErXlpgl1V7m3StPEk70X2-7U7zo";
+		$recaptcha_response = $_POST['g-recaptcha-response'];
+		
+		// Verify response of recaptcha
+		$verifylink = "https://www.google.com/recaptcha/api/siteverify";
+		$response = file_get_contents($verifylink."?secret=".$secretkey."&response=".$recaptcha_response);
+		$responseData = json_decode($response);
+		
         if(isset($_POST['type']))
 						$type=test_input($_POST['type']);
 						else $b=false;
@@ -36,44 +46,57 @@
             $id='bid';
             $tablename='login_builder';
         }
-        $q="select $id,password from $tablename where username='$username'";
-        echo $q;
-        $result=$conn->query($q);
-        if($result==true)
-        {
-            $row= mysqli_fetch_array($result,MYSQLI_ASSOC);
-        }
-        else
-        {
-					header('Location: loginuser.php');
-        }
-        if($row['password']==$password)
-        {
-            $_SESSION['username']=$username;
-            $_SESSION['type']=$type;
-            if($id=='uid' && $b==true)
-            {
-                $_SESSION['id']=$row['uid'];
-               header('Location: normalHomeSale.php');
-            }
-            if($id=='bid' && $b==true)
-            {
-                $_SESSION['id']=$row['bid'];
-                header('Location: builderHome.php');
-            }
-        }
-        else
-        {
-            echo "Invalid Password!!!!";
-            header('Location: loginuser.php');
-        }
+        if ($responseData->success) 
+		{
+			
+		
+			$q="select $id,password from $tablename where username='$username'";
+			echo $q;
+			$result=$conn->query($q);
+			if($result==true)
+			{
+				$row= mysqli_fetch_array($result,MYSQLI_ASSOC);
+			}
+			else
+			{
+						header('Location: loginuser.php');
+			}
+			if($row['password']==$password && $attemptcount < 3)
+			{
+				$attemptcount = 0;
+				$_SESSION['username']=$username;
+				$_SESSION['type']=$type;
+				if($id=='uid' && $b==true)
+				{
+					$_SESSION['id']=$row['uid'];
+				   header('Location: normalHomeSale.php');
+				}
+				if($id=='bid' && $b==true)
+				{
+					$_SESSION['id']=$row['bid'];
+					header('Location: builderHome.php');
+				}
+			}
+			else if ($attemptcount >= 3){
+				echo "<script>alert('Account Locked out');window.history.back();</script>";
+			}
+			else
+			{
+				$attemptcount++;
+				echo "<script>alert('Invalid Password!!!!'); window.location.href = 'loginuser.php';</script>";
+				header('Location: loginuser.php');
+			}
+		}
+		else {
+			echo "Invalid Captcha";
+		}
     }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <title>"housing-co"</title>
+    <title>"Login"</title>
             <meta charset="UTF-8">
             <meta name="description" content="housing-co">
             <meta name="keywords" content="LERAMIZ, unica, creative, html">
@@ -96,8 +119,8 @@
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
             <link rel="stylesheet" type="text/css" href="Styles.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+            <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+			
 
             <!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -124,15 +147,13 @@
                             <a href="https://www.instagram.com/"><i class="fa fa-instagram"></i></a>
                             <a href="https://www.pinterest.com/"><i class="fa fa-pinterest"></i></a>
                             <a href="https://www.linkedin.com/"><i class="fa fa-linkedin"></i></a>
-
-
                         </div>
-                    </div>
-                    <div class="user-panel">
-                        <a href="register.php"><i class="fa fa-user-circle-o"></i> Register(Normal User)</a>
-                        <a href="reg_builder.php"><i class="fa fa-user-circle-o"></i> Register(Builder)</a>
-                        <a href="loginuser.php"><i class="fa fa-sign-in"></i>Login</a>
-                    </div>
+						
+						<div class="user-panel">
+							<a href="register.php"><i class="fa fa-user-circle-o"></i> Register(Normal User)</a>
+							<a href="reg_builder.php"><i class="fa fa-user-circle-o"></i> Register(Builder)</a>
+						</div>
+					</div>
                 </div>
             </div>
         </div>
@@ -140,7 +161,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="site-navbar">
-                        <a href="index.html" class="site-logo"><img src="img/logo1.png" alt=""></a>
+                        <a href="index.html" class="site-logo"></a>
                         <div class="nav-switch">
                             <i class="fa fa-bars"></i>
                         </div>
@@ -240,7 +261,7 @@
 
     <br><br><br><br><br><br><br><br><br><br><br>
 
-    <form id="form" method="post" action="loginuser.php">
+    <form id="form" method="POST" action="loginuser.php">
 
         <table cellpadding="7" width="50%" border="0" align="center" cellspacing="2" color="white">
 
@@ -270,8 +291,6 @@
             </tr>
 
 
-
-
             <tr>
                 <td><b>PASSWORD:</b></td>
                 <td><input type="password" name="password" size="30">
@@ -279,7 +298,12 @@
                     <br><br>
                 </td>
             </tr>
-
+			<tr>
+				<td>
+				<div class="g-recaptcha" data-sitekey="6Le8G5QqAAAAABo02YXdJVx_0pZph0tyVcRRPgl4"></div>
+				<span class="error"></span>
+				</td>
+			</tr>
             <tr>
                 <td><b>OPTIONS:</b>
                 <td>
@@ -294,12 +318,8 @@
                 </td>
             </tr>
 
-
-
-
-
             <tr>
-                <td><input type="reset" value="Reset"></td>
+ 
                 <td><input type="submit" value="Login">
 
                     <div style="font-size:20px; color:#cc0000; margin-top:10px"></div>
